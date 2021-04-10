b73b8b7d7b0af6867b4ce646624f45deb1aeb0aa
AlekseyGricanov <50859448+AlekseyGricanov@users.noreply.github.com>
2021-04-10 23:40:44 +0700

    add server
    add server

---
+++ serverForAndroid/forAndroid/myServers/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .serializers import *
+from .models import *
+
+
+class SensorListView(generics.ListAPIView):
+    serializer_class = SensorList
+    queryset = Sensor.objects.all()
+
+
+class SensorObject(generics.RetrieveAPIView):
+    serializer_class = SensorDetailList
+    queryset = SensorDetail.objects.all()
