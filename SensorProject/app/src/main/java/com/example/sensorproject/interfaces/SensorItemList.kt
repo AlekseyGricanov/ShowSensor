245c00babd8026ced601cf8e7ba292eccc7be378
AlekseyGricanov <50859448+AlekseyGricanov@users.noreply.github.com>
2021-04-18 16:37:29 +0700

    fix SensorItemList.kt

--- SensorProject/app/src/main/java/com/example/sensorproject/interfaces/SensorItemList.kt
+++
@@ -1,4 +0,0 @@
-package com.arellomobile.mvp.sample.kotlin.interfaces
-
-class SensorItemList(var nameSensor: String, var descSensor: String, var cordSensor: String, var statusSensor: String) {
-}
---
+++ SensorProject/app/src/main/java/com/example/sensorproject/models/SensorItemList.kt
@@ -0,0 +1,8 @@
+package com.example.sensorproject.models
+
+data class SensorItemList(
+    var nameSensor: String,
+    var descSensor: String,
+    var cordSensor: String,
+    var statusSensor: String
+)
