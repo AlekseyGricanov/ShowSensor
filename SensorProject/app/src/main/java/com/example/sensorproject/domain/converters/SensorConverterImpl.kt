f2bcc20c80af2cea15e31653b44ef5f6c2467b2c
AlekseyGricanov <50859448+AlekseyGricanov@users.noreply.github.com>
2021-05-09 10:59:36 +0700

    update version

--- SensorProject/app/src/main/java/com/example/sensorproject/domain/converters/SensorConverterImpl.kt
+++ SensorProject/app/src/main/java/com/example/sensorproject/domain/converters/SensorConverterImpl.kt
@@ -1,11 +1,13 @@
 package com.example.sensorproject.domain.converters
 
+import com.example.sensorproject.data.remote.models.AdvSensorApi
 import com.example.sensorproject.data.remote.models.SensorApi
 import com.example.sensorproject.domain.models.SensorItemList
+import com.example.sensorproject.domain.models.SensorList
 
 class SensorConverterImpl {
 
-    fun fromApiToUI(model: SensorApi): SensorItemList {
+    fun fromApiToUISensor(model: SensorApi): SensorItemList {
         return SensorItemList(nameSensor = model.name, descSensor = model.desc, cordSensor = model.coordinates, statusSensor = model.status)
     }
 }
